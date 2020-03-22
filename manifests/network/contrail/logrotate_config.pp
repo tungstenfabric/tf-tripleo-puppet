bb1cc9f31cb39f76a60081158e4e154c23644d20
Mitya_Eremeev <mitossvyaz@gmail.com>
2020-03-22 21:27:49 +0300

    /var/log/containers/contrail/ permissions.
    Provide TF logrotate settings for Openstack logrotate.
    
    Closes-jira-bug: CEM-10470
    Change-Id: Ie2c0e1cfb1c5689b35bca7841b0f1559f09bd9a7

---
+++ manifests/network/contrail/logrotate_config.pp
@@ -0,0 +1,54 @@
+class tripleo::network::contrail::logrotate_config (
+  $step             = Integer(hiera('step')),
+  $ensure           = present,
+  $minute           = 0,
+  $hour             = '*',
+  $monthday         = '*',
+  $month            = '*',
+  $weekday          = '*',
+  Integer $maxdelay = 90,
+  $user             = 'root',
+  $log_path_regex   = '/var/log/containers/contrail/*log /var/log/containers/contrail/*err',
+  $rotate_user_opts = 'su root root',
+  $config           = '/etc/logrotate-crond.conf',
+  $contrail_config  = '/etc/contrail-logrotate.conf',
+  $copytruncate     = true,
+  $delaycompress    = true,
+  $compress         = true,
+  $rotation         = 'daily',
+  $maxsize          = '10M',
+  $rotate           = 14,
+  $purge_after_days = 14,
+  $dateext          = undef,
+  $dateformat       = undef,
+  $dateyesterday    = undef,
+  # DEPRECATED PARAMETERS
+  $size             = undef,
+) {
+
+# we add contrail config section on next step after config creation on step >= 4
+# https://github.com/openstack/puppet-tripleo/blob/stable/queens/manifests/profile/base/logging/logrotate.pp#L124
+# TODO: don't guess wright step to edit config after its creation, do global depending.
+# TODO: do edit config file if there is hiera item: contrail log path regex.
+  if $step == 5 {
+    if ($size != undef) {
+      warning('The size parameter is DISABLED to enforce GDPR.')
+      warning('Size configures maxsize instead of size.')
+      $maxsize = pick($size, $maxsize)
+    }
+
+    file { "${contrail_config}":
+      ensure  => file,
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+      content => template('tripleo/contrail/contrail_logrotate_config.conf.erb'),
+    } ->
+
+    exec { "add_openstack_section_to_new_config":
+      command => "cat '${config}' >> '${contrail_config}' && mv -f '${contrail_config}' '${config}'",
+      onlyif => "test -f '${config}'",
+      path => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+    }
+  }
+}
